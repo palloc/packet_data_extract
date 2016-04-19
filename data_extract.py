d94369c656680ecaada8b8dbea4fd87dfd0d1fcc
palloc <palloc.mknet10@gmail.com>
2016-04-19 11:37:45 +0900

    tested mac_dst

--- data_extract.py
+++ data_extract.py
@@ -4,7 +4,7 @@ from scapy.all import *
 def show_all(packet):
     for i in packet:
         print packet.show()
-
+#export source mac address
 def export_mac_src(packet,output):
     file = open(output,'w')
     s = ""
@@ -12,12 +12,15 @@ def export_mac_src(packet,output):
         s += i.src + '\n'
     file.write(s)
 
+#export destination mac address
 def export_mac_dst(packet,output):
     file = open(output,'w')
     s = ""
     for i in packet:
         s += i.dst + '\n'
     file.write(s)
+
+#export protocol number
 def export_protocol(packet,output):
     file = open(output,'w')
     s = ""
@@ -31,11 +34,13 @@ def export_protocol(packet,output):
 
     
 if __name__ == "__main__":
+    packet_data = rdpcap("data/sample1.pcap")
+    output_name = "data/test"
+
 #    f_name = raw_input("enter pcap file name:")
 #    output_name = raw_input("enter output file name:")
 #    packet_data = rdpcap(f_name)
 #    export_mac_src(packet_data,output_name)
-    packet_data = rdpcap("data/sample1.pcap")
-    output_name = "data/test"
+#    export_protocol(packet_data,output_name)
+    export_mac_dst(packet_data,output_name)
 
-    export_protocol(packet_data,output_name)
